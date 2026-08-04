79172115ad0c1510eb5ee4146cb103875b9aa392
Gomesyrn <josepedrogomes2010@hotmail.com>
2026-08-04 20:27:05 +0100

    fix: usa hashrouter para corrigir rotas no github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
-import Quinta from "./Quinta.jsx"; // Importa o componente da Quinta
-import Alojamento from "./Alojamento.jsx"; // Importa o componente do Alojamento
+import Quinta from "./Quinta.jsx";
+import Alojamento from "./Alojamento.jsx";
 import Experiencias from "./Experiencias.jsx";
-import Galeria from "./Galeria.jsx"; // Importa o componente da Quinta
-import Contactos from "./Contactos.jsx"; // Importa o componente da Quinta
+import Galeria from "./Galeria.jsx";
+import Contactos from "./Contactos.jsx";
 import "./index.css";
 import "./i18n.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/a-quinta" element={<Quinta />} />{" "}
-        <Route path="/alojamento" element={<Alojamento />} />{" "}
-        <Route path="/galeria" element={<Galeria />} />{" "}
-        <Route path="/experiencias" element={<Experiencias />} />{" "}
-        <Route path="/contactos" element={<Contactos />} />{" "}
+        <Route path="/a-quinta" element={<Quinta />} />
+        <Route path="/alojamento" element={<Alojamento />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/experiencias" element={<Experiencias />} />
+        <Route path="/contactos" element={<Contactos />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
